8d6f5a3282b12b5335f4aa9aa8b69f1fdf8bd7ba
VibeProjectVCD <140895716+VibeProjectVCD@users.noreply.github.com>
2024-08-03 20:55:36 +0300

    Add files via upload

---
+++ nyx-main/libs/console.lua
@@ -0,0 +1,37 @@
+-- what script actually uses these shit lol
+--https://synapsexdocs.github.io/custom-lua-functions/console-functions/
+function rconsoleclear() --Clears the console. MACRO: system("CLS")
+end
+
+function rconsolecreate() --todo: allocate console via bridge??? possible but i dont want to do this
+end
+
+function rconsoledestroy() -- in cpp i would do ShowWindow(hwnd, SW_HIDE) to destroy if we want we can just close it 
+end
+
+function rconsoleinput() 
+end
+
+function rconsoleprint(txt) 
+end
+
+function consoleclear() 
+end
+
+function consolecreate() 
+end
+
+function consoledestroy() 
+end
+
+function consoleinput() 
+end
+
+function consoleprint(txt) 
+end
+
+function rconsolename(txt) 
+end
+
+function consolesettitle(txt) 
+end
